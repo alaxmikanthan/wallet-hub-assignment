8e52ce2b5a884b28a10bbf4ddc0cfbe2937de363
arunkanthan <arunkanthan@gmail.com>
2018-10-15 16:21:39 +0530

    added capability foir chrome

--- src/main/java/com/qa/automation/core/CommonPageHelpersLib.java
+++ src/main/java/com/qa/automation/core/CommonPageHelpersLib.java
@@ -3,9 +3,12 @@ package com.qa.automation.core;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Window;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -18,6 +21,7 @@ public class CommonPageHelpersLib {
 	public static WebElement we;
 	public static FirefoxOptions ffoptions;
 
+	@SuppressWarnings("deprecation")
 	public static void init() {
 
 		if (Config.browser == "Firefox") {
@@ -27,9 +31,14 @@ public class CommonPageHelpersLib {
 			capabilities.setCapability(FirefoxDriver.PROFILE, profile);
 			System.setProperty("webdriver.gecko.driver", Config.firefoxdriverpath);
 			driver = new FirefoxDriver(capabilities);
+		} else if (Config.browser == "Chrome") {
+			System.setProperty("webdriver.chrome.driver", Config.chromedriverpath);
+			driver = new ChromeDriver();
 		}
 		driver.manage().timeouts().implicitlyWait(Config.timeout, TimeUnit.SECONDS);
 		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		
 	}
 
 	public static void get(String url) {
--- src/main/java/com/qa/automation/core/Config.java
+++ src/main/java/com/qa/automation/core/Config.java
@@ -1,7 +1,7 @@
 package com.qa.automation.core;
 
 public class Config {
-	protected static String browser = "Firefox";
+	protected static String browser = "Chrome";
 	protected static String firefoxdriverpath = "C:\\selenium\\geckodriver.exe";
 	protected static String chromedriverpath = "C:\\selenium\\chromedriver.exe";
 	protected static int timeout = 30;
