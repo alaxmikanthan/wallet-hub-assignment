060cee5ee82369559cf67a358cd98e55914b2e37
arunkanthan <arunkanthan@gmail.com>
2018-10-15 16:26:58 +0530

    disable chrome notifications and changed config to use firefox

--- src/main/java/com/qa/automation/core/CommonPageHelpersLib.java
+++ src/main/java/com/qa/automation/core/CommonPageHelpersLib.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver.Window;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -32,8 +33,10 @@ public class CommonPageHelpersLib {
 			System.setProperty("webdriver.gecko.driver", Config.firefoxdriverpath);
 			driver = new FirefoxDriver(capabilities);
 		} else if (Config.browser == "Chrome") {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--disable-notifications");
 			System.setProperty("webdriver.chrome.driver", Config.chromedriverpath);
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(options);
 		}
 		driver.manage().timeouts().implicitlyWait(Config.timeout, TimeUnit.SECONDS);
 		driver.manage().deleteAllCookies();
